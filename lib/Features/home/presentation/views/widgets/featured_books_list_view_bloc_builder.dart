e193e3e1d3a6d7334f9c8bf931d1c9dbcf1da709
smgr9 <mohamadgazer@gmail.com>
2023-07-28 11:51:29 +0300

    end featured books bloc butlder

--- lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/Features/home/presentation/manger/feature_book_cubit/feature_book_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListviewBlocBuilder extends StatelessWidget {
   const FeaturedBooksListviewBlocBuilder({
@@ -8,6 +10,16 @@ class FeaturedBooksListviewBlocBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FeaturedBooksListView();
+    return BlocBuilder<FeatureBookCubit, FeatureBookState>(
+      builder: (context, state) {
+        if (state is FeatureBookSuccess) {
+          return const FeaturedBooksListView();
+        } else if (state is FeatureBookFailure) {
+          return Text(state.errMessage);
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
