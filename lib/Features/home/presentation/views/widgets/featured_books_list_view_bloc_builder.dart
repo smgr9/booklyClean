a27531baa4c537f30c308357d404afaa4ee880d6
smgr9 <mohamadgazer@gmail.com>
2023-07-28 11:45:23 +0300

    start featured books list view bloc builder

---
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListviewBlocBuilder extends StatelessWidget {
+  const FeaturedBooksListviewBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const FeaturedBooksListView();
+  }
+}
