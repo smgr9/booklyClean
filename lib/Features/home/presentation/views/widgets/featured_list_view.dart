2c5c45a2e03291ab5cec0f6cf6457f3afb881731
smgr9 <mohamadgazer@gmail.com>
2023-07-28 12:14:51 +0300

    edit FeaturedBooksListView

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -12,6 +12,7 @@ class FeaturedBooksListView extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
+          itemCount: books.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
             return Padding(
